387663f5b3c431a30a936bbc7d6c50b4fc87700d
mffnx <karinakh1802@gmail.com>
2025-12-02 02:16:01 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/frontend/sw.js', { scope: '/frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
