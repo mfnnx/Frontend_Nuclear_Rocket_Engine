bb7c851e37401487773cde4d5db05e1a75edc732
mffnx <karinakh1802@gmail.com>
2025-12-13 18:54:02 +0300

    fixed-adaptability

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.p60p9jg7urk"
+    "revision": "0.1lssvrku5q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
